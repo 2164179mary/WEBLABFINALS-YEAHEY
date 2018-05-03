896aa3a3777997c2b3a2c1e3f0ad86a2e5cfbf82
Melody-Molina <35625798+Melody-Molina@users.noreply.github.com>
2018-05-03 17:48:18 +0800

    updated the accept and deny module.

--- web/php/FetchDB.php
+++ web/php/FetchDB.php
@@ -5,10 +5,10 @@ if ($conn->connect_error) die($conn->connect_error);
 
 /*if (isset($_POST['accepted']) && isset($_POST['status'])){
     $status = get_post($conn, 'status');
-    $query
+    $query = "UPDATE ";*/
 
 
-}*/
+
 //fetching data in mysql
 $query = 'select username, fname, lname, typeAccount, status  from account join customer on username = customerID where status=\'pending\' union
           select username, fname, lname, typeAccount, status  from account inner join sp on username = spID where status=\'pending\'; ';
@@ -20,14 +20,16 @@ $rows = $result->num_rows;
 
 for ($j = 0; $j < $rows; ++$j){
     $result->data_seek($j);
-   $row = $result->fetch_array(MYSQLI_NUM);
+   $row = $result->fetch_array(MYSQLI_BOTH);
 
- /*   echo 'Username: ' .$row['username']  .'<br>';
+  /*echo 'Username: ' .$row['username']  .'<br>';
     echo 'Name: '     .$row['fname']. " " .$row['lname']. '<br>';
     echo 'Account: '  .$row['typeAccount']. '<br>';
     echo 'Status: '   .$row['status'].'<br> <br>';*/
 
     echo <<<_END
+    <!Doctype html>
+    <body>
     <pre>
     Username: $row[0]
     Given Name: $row[1]
@@ -36,15 +38,17 @@ for ($j = 0; $j < $rows; ++$j){
     Status: $row[4]
     </pre>
     
-   <form action = "FetchDB.php" method="post">
+   <form action = "_____.php" method="post">
    <input type = "hidden" name = "deny" value="yes">
    <input type = "hidden" name= "status" value="$row[4]">
    <input type="submit" value="Deny Request">
    
-   <input type = "hidden" name = "accept" value="yes">
+   <input type = "hidden" name = "accepted" value="yes">
    <input type = "hidden" name= "status" value="$row[4]">
    <input type="submit" value="Accept Request">
 </form>
+</body>
+</html>
 
 _END;
 
@@ -56,3 +60,9 @@ _END;
 $result->close();
 $conn->close();
 
+//gawin sa table
+//update.php
+//s$typeee
+/*update customer (type)
+set status = 'accepted'
+where (type)customerID='3';*/
