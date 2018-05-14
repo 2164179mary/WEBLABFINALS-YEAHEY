c37395640225811612e24528a07245835273cb66
2152093 <33900851+2152093@users.noreply.github.com>
2018-05-14 21:32:52 +0800

    Final Transaction Monitoring

--- web/php/ServiceMonitoring.php
+++ web/php/ServiceMonitoring.php
@@ -1,6 +1,6 @@
 
 <?php
-
+session_start();
 //connect database
 
 require_once 'connectDB.php';
@@ -12,21 +12,23 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT serviceName, spID, gender FROM service;";
+$sql = "SELECT serviceName,image, spID, gender FROM service;";
 $result = $conn->query($sql);
+
 if ($result->num_rows > 0) {
     // output data of each row
+    $increment = 0;
     while($row = $result->fetch_assoc()) {
-        echo "<div>";
+        echo "<div style=\"border: 1px solid black;\">";
+        $increment = $increment + 1 ;
         echo "<br>";
         echo "<br>";
-        echo $row["image"];
+        echo "<img src=\"data:image/jpeg;base64," .  base64_encode( $row['image'] ) . "\">";
         echo "Service Name: " . $row["serviceName"] . "<br>";
         echo " spID : " . $row["spID"] . "<br>";
         echo " Gender: " . $row["gender"] . "<br>";
-        echo "<a href=\"TransactionM.php\">View Transaction<a/>";
-        session_start();
         $_SESSION['spID'] = $row["spID"];
+        echo "<a href=\"TransactionM.php?content=".$_SESSION['spID']."\">View Transaction</a>";
         echo "</div>";
     }
 } else {
--- web/php/TransactionM.php
+++ web/php/TransactionM.php
@@ -1,22 +1,23 @@
 <?php
+session_start();
 require_once 'connectDB.php';
 
 $conn = new mysqli($lh, $un, $pw, $db);
 
-
-session_start();
-$var_value = $_SESSION['SPID'];
+$var_value = isset($_GET['content']) ? $_GET['content'] : '';
 
 $query = "select spID, contractBill, Status from sp where (spID = \"$var_value\" );";
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
+        echo "<div style=\"border: 1px solid black;\">";
         echo "<br>";
         echo "<br>";
         echo " ServiceProvider : " . $row["spID"] . "<br>";
         echo " Contract Bill : " . $row["contractBill"] . "<br>";
         echo " Status: " . $row["Status"] . "<br>";
+        echo "</div>";
     }
 } else {
     echo "0 results";
